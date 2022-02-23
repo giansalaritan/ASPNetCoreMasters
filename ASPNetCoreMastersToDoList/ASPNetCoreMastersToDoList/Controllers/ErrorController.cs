e7dd0055a0b68aed98906787ea12e9f6380c4fa9
Gian Salaritan <GianSa@magenic.com>
2022-02-24 04:17:35 +0800

    Session 2

---
+++ ASPNetCoreMastersToDoList/ASPNetCoreMastersToDoList/Controllers/ErrorController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetCoreExceptionHandler.Controllers;
+
+[ApiController]
+public class ErrorController : ControllerBase
+{
+    [Route("/error")]
+    public IActionResult Error() => Problem();
+}
---
+++ ASPNetCoreMastersToDoList/ASPNetCoreMastersToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace ASPNetCoreMastersToDoList.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        public IActionResult Get(int userId)
+        {
+            var strings = ItemService.GetAll(userId);
+            return Ok(strings);
+        }
+    }
+}

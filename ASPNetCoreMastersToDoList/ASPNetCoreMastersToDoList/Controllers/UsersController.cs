b2c0829f1e327eadadb17dbbbebb6a26b56064c4
Gian Salaritan <GianSa@magenic.com>
2022-03-20 22:43:17 +0800

    Session 6

---
+++ ASPNetCoreMastersToDoList/ASPNetCoreMastersToDoList/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using ASPNetCoreMastersToDoList.ConfigModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ASPNetCoreMastersToDoList.Controllers
+{
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly JWTConfigModel _jwt;
+        public UsersController(IOptions<JWTConfigModel> options)
+        {
+            _jwt = options.Value;
+        }
+        [HttpGet]
+        [Route("/login")]
+        public IActionResult Login()
+        {
+            return Ok(_jwt);
+        }
+    }
+}
